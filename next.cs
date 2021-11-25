eebf4ca2db075b925a0c26114e31fa0b455dc19c
miasmist <81637913+miasmist@users.noreply.github.com>
2021-11-25 20:44:27 +0000

    Update next.cs

--- next.cs
+++ next.cs
@@ -1,3 +1,5 @@
+//Loads next level
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,15 +17,5 @@ public class next : MonoBehaviour
         LoadNextLevel();
         
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
