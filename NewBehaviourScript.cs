c63630c683c6ce02c0b56fe26645ab8bceccb3f6
miasmist <81637913+miasmist@users.noreply.github.com>
2021-11-25 20:39:06 +0000

    Update NewBehaviourScript.cs

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    //public Transform CameraRotation;
-    //public Transform MainObject;
-    //public Transform Camera;
-    //public AudioSource FootSteps;
+    
     public bool JumpAbility = false;
     public float strafe = 50f;
     public float Forward = 75f;
@@ -22,50 +19,38 @@ public class NewBehaviourScript : MonoBehaviour
     private float StrafeSpeedNeuter;
     public Rigidbody rb;
     
-    //CharacterController characterController;
+    
     
     
     private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log("I am extremely emasculate");
+        //This form of testing to see if the player can jump is pretty terrible, may change in the future
         JumpAbility = true;
     }
-    // Start is called before the first frame updateS
+    
     void Start()
     {
-        //FootSteps = GetComponent<AudioSource>();
-        //GetComponent.Source>().Play();
-        //GetComponent<AudioSource>().Pause();
+        
         rb = GetComponent<Rigidbody>();
-        //CameraRotation = GetComponent<Transform>();
-        //MainObject = GetComponent<Transform>();
-        //characterController = GetComponent<CharacterController>();
         
     }
     void Update()
     {
         CurrSpeed = rb.velocity.magnitude;
-        SpeedLock = (MaxSpeed - CurrSpeed)/MaxSpeed;
-        //float SpeedLock = maxSpeed - speed;
-        //SpeedLock = SpeedLock/maxSpeed;
-        //if (JumpAbility == true)
-        //{
-            //GetComponent<AudioSource>().UnPause();
-        //}
-        //else
-        //{
-          //  GetComponent<AudioSource>().Pause();
-        //}
+        SpeedLock = (MaxSpeed - CurrSpeed)/MaxSpeed; //Makes it so that if they player is travelling faster than
+        //a specific limit, the effect they have in game is lessened, in a way that soft caps the player, instead of a
+        //harsh limit
+        
         if (Input.GetKey("escape"))
         {
-            Application.Quit();
+            Application.Quit(); //closes game when built
         }
-        if (Input.GetKeyDown(KeyCode.Space) && JumpAbility == true)
+        if (Input.GetKeyDown(KeyCode.Space) && JumpAbility == true) //Done in update due to clashes with other inputs 
         {
             rb.AddForce(0, Jumping, 0* Time.deltaTime);
             JumpAbility = false;
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))    //Slows player down when crouching
         {
             CrouchSpeed = 0.4f;
         }
@@ -74,14 +59,11 @@ public class NewBehaviourScript : MonoBehaviour
             CrouchSpeed = 1f;
         }
     }
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
-        //CameraRotation.rotation.y = MainObject.rotation.y;
-        //MainObject.rotation = Quaternion.LookRotation(-Camera.main.transform.forward, Camera.main.transform.up);
-        //MainObject.transform.rotation = Quaternion.Euler(eulerRotation);
         
-        if (JumpAbility != true)
+        if (JumpAbility != true)    //Player has less maneouverability in the air, but strafing is still relatively stron (source)
         {
             AirSpeedNeuter = 0.25f;
             StrafeSpeedNeuter = 0.8f;
@@ -115,12 +97,12 @@ public class NewBehaviourScript : MonoBehaviour
             rb.AddRelativeForce(strafe * StrafeSpeedNeuter * SpeedLock * CrouchSpeed,0,0 * Time.deltaTime);
             
         }
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))    //Gives the player more maneouverability, as crouch also pushes down
         {
             rb.AddRelativeForce(0,-CrouchDown,0);
         }
         
-        rb.AddForce(0,-Gravity,0);
-        //rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        rb.AddForce(0,-Gravity,0);  //Player falls faster and jumping feels more realisitic
+        
     }
 }
