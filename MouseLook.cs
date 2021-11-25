c0e9377e98e9c4fcce04cc1e97506fb58d5634a2
miasmist <81637913+miasmist@users.noreply.github.com>
2021-11-25 20:32:45 +0000

    Update MouseLook.cs

--- MouseLook.cs
+++ MouseLook.cs
@@ -1,13 +1,18 @@
+//handles Looking and Shooting, instantiates different things etc
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100.0f;
-    public float clampAngle = 87.5f;
-    private float rotY = 0.0f; // rotation around the up/y axis
-    private float rotX = 0.0f; // rotation around the right/x axis
+    
+    public Transform playerBody;    //Reference to transform of the player "collider"
+    public float mouseSensitivity = 100.0f; //sens
+    public float clampAngle = 87.5f;    //clamp so that player can look further than clampAngle degrees
+    private float rotY = 0.0f; 
+    private float rotX = 0.0f;
+    //Following variables handle Explosion Instantiation, different variables in the process
     public float BoomStrength = 10.0f;
     public float BoomRadius = 5.0f;
     public float BoomLift = 3.0f;
@@ -15,12 +20,12 @@ public class MouseLook : MonoBehaviour
     public bool canShoot = true;
     public float coolDown = 5.0f;
     public Rigidbody Reginald;
-    public Transform playerBody;
     public GameObject Particle;
     public GameObject BoomParticle;
     
     void Start()
     {
+        //sets out variables so that script can access camera rotation and apply them to the player "collider"
         Vector3 rot = transform.localRotation.eulerAngles;
         rotY = rot.y;
         rotX = rot.x;
@@ -29,6 +34,7 @@ public class MouseLook : MonoBehaviour
     }
     void CooledDown()
     {
+        //Make it so that can only "shoot" in periods
         canShoot = true;
     }
     void SpawnBoom()
@@ -59,7 +65,7 @@ public class MouseLook : MonoBehaviour
     }
     void Update()
     {
-        if (!Input.GetKey(KeyCode.LeftShift))
+        if (!Input.GetKey(KeyCode.LeftShift))   //attempt at implementation of shift-to-rocketjump from source in unity
         {
             BoomModifier = 1.0f;
         }
@@ -67,33 +73,29 @@ public class MouseLook : MonoBehaviour
         {
             BoomModifier = 1.75f;
         }
-        float mouseX = Input.GetAxis("Mouse X");
+        float mouseX = Input.GetAxis("Mouse X");    //Gets mouse input
         float mouseY = -Input.GetAxis("Mouse Y");
-        rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        rotX += mouseY * mouseSensitivity * Time.deltaTime;
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        rotY += mouseX * mouseSensitivity * Time.deltaTime; //Can use mouse input to affect camera  
+        rotX += mouseY * mouseSensitivity * Time.deltaTime; 
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);  //Applies the clamping 
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f); //Applies rotation to camera
         transform.rotation = localRotation;
         Vector3 eulerRotation = new Vector3(playerBody.transform.eulerAngles.x, transform.eulerAngles.y, playerBody.transform.eulerAngles.z );
+        //^This applies the y rotation to the player so that forces can be applied in same direction as camera
         
         playerBody.rotation = Quaternion.Euler(eulerRotation);
         if (Input.GetKey(KeyCode.Mouse0))
         {
             Instantiate(Particle, CastRay().point, Quaternion.Euler(270,0,0));
-            
-            //Instantiate(Bullet,);
-            //Destroy(Particle);
+              
         }
         
         if (Input.GetKey(KeyCode.Mouse1) && canShoot)
         {
-            //Instantiate(BoomParticle, CastRay().point, Quaternion.Euler(270,0,0));
-            //Reginald.AddExplosionForce(BoomStrength, CastRay().point, BoomRadius, BoomLift);
             SpawnBoom();
             canShoot = false;
             Invoke("CooledDown", coolDown);
-            //Instantiate(Bullet,);
-            //Destroy(Particle);
+            //"Rocket Jumping" can only shoot every specific time period
         }
     }
 }
