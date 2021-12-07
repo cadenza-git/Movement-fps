d03fc36a1d62a1cc2b278d2c4b96144a82247cac
miasmist <81637913+miasmist@users.noreply.github.com>
2021-12-07 21:26:35 +0000

    Displays life left and is more readable

--- fps.cs
+++ fps.cs
@@ -15,20 +15,34 @@ public class fps : MonoBehaviour
     public Text SpeedText;
     public Rigidbody player;
     private string TellMePls;
+    private float start = 0.01f;
+    private float period = 0.1f;
     
     public void Start ()
     {
         Mlook = FPSCAM.GetComponent<MouseLook>(); //References canShoot from MouseLook for use.
+        LifeScript = Player.GetComponent<NewBehaviourScript>();
+        InvokeRepeating("HUD", start, period);//updates the HUD every period seconds
     }
-    public void Update ()
+    
+    void HUD()
     {
-        
+    
+        float ShowSpeed = player.velocity.magnitude;
+        int IntSpeed = (int)Mathf.Round(ShowSpeed);
         float current = 0;
-        float ShowSpeed = player.velocity.magnitude; //Gets Speed
-        current = (int)(1f / Time.unscaledDeltaTime); //Gets FPS
+        
+        current = (int)(1f / Time.unscaledDeltaTime);//DeltaTime returns a float between 0 and 1, related to fps, divide 1 by it to get proper number
         avgFrameRate = (int)current;
         display_Text.text = avgFrameRate.ToString() + " FPS ";
-        SpeedText.text = ShowSpeed.ToString();
+        
+        SpeedText.text = IntSpeed.ToString();
+    }
+    
+    public void FixedUpdate ()
+    {
+        
+        LifeText.text = LifeScript.life.ToString; //displays how much "life" is left
         if (!Mlook.canShoot)
         {
             //EnableShootTell.enabled = false;(usable if using an icon)
