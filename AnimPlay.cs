a2a2e7f7c96629dcb274bf6695383dfdf47f31fc
cadenza <81637913+cadenza-git@users.noreply.github.com>
2021-12-13 18:57:33 +0000

    File to handle animations on different gameobjects

---
+++ AnimPlay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPlay : MonoBehaviour
+{
+    public Animator nimChim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nimChim.Play("shid");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
