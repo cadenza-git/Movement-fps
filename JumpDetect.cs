1178e72e4754f6c638ead9ba6583ab4ae408a89b
cadenza <81637913+cadenza-git@users.noreply.github.com>
2021-12-15 19:24:30 +0000

    Handed over Jump Detection, cleaner for unity

--- JumpDetect.cs
+++ JumpDetect.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class JumpDetect : MonoBehaviour
 {
+    public AudioSource Jump;
+    public Rigidbody rigb;
     public bool CanJump;
-    // Start is called before the first frame update
+    public float Jumping = 1225f;
+    public GameObject Player;
+    private NewBehaviourScript playerscript;
+    
+    void Start()
+    {
+        playerscript = Player.GetComponent<NewBehaviourScript>();
+        CanJump = playerscript.JumpAbility; 
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump == true)
+        {
+            rigb.AddForce(0, Jumping, 0* Time.deltaTime);
+            Jump.Play();
+            CanJump = false;
+        }
+    }
+    
+    void CantJump()
+    {
+        if(CanJump)
+        {
+            CanJump = false;
+        }
+    }
+    
     void OnTriggerEnter()
     {
         CanJump = true;
     }
 
+    void OnTriggerStay()
+    {
+        CanJump = true;
+    }
     void OnTriggerExit()
     {
-        CanJump = false;
+        Invoke("CantJump",0.2f);
+        
     }
 }
