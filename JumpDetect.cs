6992b05714141cc1d6b2d48b831842b995baa90a
cadenza <81637913+cadenza-git@users.noreply.github.com>
2021-12-23 13:31:21 +0000

    Migrated WallRun implemented stronger walljump

--- JumpDetect.cs
+++ JumpDetect.cs
@@ -7,22 +7,47 @@ public class JumpDetect : MonoBehaviour
     public AudioSource Jump;
     public Rigidbody rigb;
     public bool CanJump;
+    public float SpeedLock;
     public float Jumping = 1225f;
+    public float WallRunY = 63;
+    public float WallRunForward = 265;
+    private float JumpAirModifier = 1.0f;
+    private float JumpDirect = 1.0f;
     public GameObject Player;
     private NewBehaviourScript playerscript;
     
     void Start()
     {
         playerscript = Player.GetComponent<NewBehaviourScript>();
-        CanJump = playerscript.JumpAbility; 
     }
     void Update()
     {
+        SpeedLock = playerscript.SpeedLock; 
+        
         if (Input.GetKeyDown(KeyCode.Space) && CanJump == true)
         {
-            rigb.AddForce(0, Jumping, 0* Time.deltaTime);
+            rigb.AddForce(0, Jumping *JumpAirModifier, 1*JumpDirect);
+            JumpAirModifier = 1.0f;
+            JumpDirect = 1.0f;
             Jump.Play();
             CanJump = false;
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        if(playerscript.CheckWallRun() && Input.GetKey(KeyCode.W) && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) ) //Wallrun Controller
+        {
+            Debug.Log("Shat meself");
+            rigb.AddRelativeForce(0,WallRunY,WallRunForward*SpeedLock);
+            CanJump = true;
+            JumpAirModifier = 0.05f;
+            JumpDirect = 1.0f;
+        }
+        if(!playerscript.CheckWallRun())
+        {
+            JumpAirModifier = 1.8f;
+            JumpDirect = 300.0f;
         }
     }
     
@@ -31,17 +56,24 @@ public class JumpDetect : MonoBehaviour
         if(CanJump)
         {
             CanJump = false;
+            JumpAirModifier = 1.0f;
+            JumpDirect = 1.0f;
         }
     }
     
     void OnTriggerEnter()
     {
+        JumpDirect = 1.0f;
         CanJump = true;
+        JumpAirModifier = 1.0f;
+        
     }
 
     void OnTriggerStay()
     {
+        JumpDirect = 1.0f;
         CanJump = true;
+        JumpAirModifier = 1.0f;
     }
     void OnTriggerExit()
     {
